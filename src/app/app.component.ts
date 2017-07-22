6ba3120679acc864cc908bd81d23a13563ad4041
flavglen <flavglen@gmail.com>
2017-07-22 14:31:05 +0530

    initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit,} from '@angular/core';
+import {TreeNode} from 'primeng/primeng';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent   implements OnInit{
+
   title = 'app';
+  cars: any[];
+  data: TreeNode[];
+
+  ngOnInit() {
+    this.data = [{
+          label: 'Integrated',
+          expanded: true,
+          children: [
+                       {
+                           label: 'Montessari'
+                       },
+                       {
+                           label: 'Kindergarten'
+                       }
+                   ]
+
+         }];
+
+    this.cars = [
+         {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
+         {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
+         {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
+         {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'}
+       ]
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {CarouselModule, OrganizationChartModule,ButtonModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,CarouselModule,OrganizationChartModule,BrowserAnimationsModule,ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
