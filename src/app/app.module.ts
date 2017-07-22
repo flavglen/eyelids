e0b78caa0eaed9639b2e5dd01e34ad01cd68165e
flavglen <flavglen@gmail.com>
2017-07-22 18:13:56 +0530

    added slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import {CarouselModule, OrganizationChartModule,ButtonModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SliderComponentComponent } from './slider-component/slider-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponentComponent
   ],
   imports: [
     BrowserModule,CarouselModule,OrganizationChartModule,BrowserAnimationsModule,ButtonModule
